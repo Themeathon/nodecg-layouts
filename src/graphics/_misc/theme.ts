09fb71a74050ffcd1d1fc395e5077af537099a25
FreezyLemon <contact@freezylemon.com>
2024-12-01 11:26:49 +0100

    Remove non-themeathon themes
    These seem to drop files into the output,
    whether they're used or not

--- src/graphics/_misc/theme.ts
+++ src/graphics/_misc/theme.ts
@@ -2,26 +2,9 @@
 
 const { theme } = nodecg.bundleConfig.event;
 const { default: defaultTheme } = require('./themes/default.theme.css');
-const { default: together } = require('./themes/together.theme.css');
-const { default: coronarelief } = require('./themes/coronarelief.theme.css');
-const { default: uksg } = require('./themes/uksg.theme.css');
-const { default: uksgsp } = require('./themes/uksgsp.theme.css');
-const { default: uksgsu } = require('./themes/uksgsu.theme.css');
-const { default: uksga } = require('./themes/uksga.theme.css');
-const { default: uksgw } = require('./themes/uksgw.theme.css');
-const { default: uksgred } = require('./themes/uksgred.theme.css');
-const { default: uksggreen } = require('./themes/uksggreen.theme.css');
-const { default: uksgblue } = require('./themes/uksgblue.theme.css');
-const { default: summer } = require('./themes/summer.theme.css');
-const { default: winter } = require('./themes/winter.theme.css');
-const { default: legends } = require('./themes/legends.theme.css');
-const { default: swcf } = require('./themes/swcf.theme.css');
-const { default: nsgspr24 } = require('./themes/nsgspr24.theme.css');
 const { default: themeat24 } = require('./themes/themeat24.theme.css');
 const { default: themeat_hal24 } = require('./themes/themeat-hal24.theme.css');
 const { default: themeat_ww24 } = require('./themes/themeat-ww24.theme.css');
-
-if (theme !== 'themeat-ww24') defaultTheme.use();
 
 switch (theme) {
   case 'themeat-ww24':
@@ -33,55 +16,6 @@ switch (theme) {
   case 'themeat24':
     themeat24.use();
     break;
-  case 'nsgspr24':
-    nsgspr24.use();
-    break;
-  case 'together':
-    together.use();
-    break;
-  case 'coronarelief':
-    coronarelief.use();
-    break;
-  case 'uksgsp':
-    uksg.use();
-    uksgsp.use();
-    break;
-  case 'uksgsu':
-    uksg.use();
-    uksgsu.use();
-    break;
-  case 'uksga':
-    uksg.use();
-    uksga.use();
-    break;
-  case 'uksgw':
-    uksg.use();
-    uksgw.use();
-    break;
-  case 'uksgred':
-    uksg.use();
-    uksgred.use();
-    break;
-  case 'uksggreen':
-    uksg.use();
-    uksggreen.use();
-    break;
-  case 'uksgblue':
-    uksg.use();
-    uksgblue.use();
-    break;
-  case 'summer':
-    summer.use();
-    break;
-  case 'winter':
-    winter.use();
-    break;
-  case 'swcf':
-    swcf.use();
-    break;
-  case 'legends':
-    legends.use();
-    break;
   default:
-    // do nothing
+    defaultTheme.use();
 }
